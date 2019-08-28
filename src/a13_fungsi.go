b1b8de1799a7aca661ba50c8a39a2bc0795fb049
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-08-28 16:11:06 +0700

    random

--- src/a13_fungsi.go
+++ src/a13_fungsi.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func setStringMessages(arr []string) {
 	//ini mirip implode di php cuk XD
 	var localString = strings.Join(arr, " ")
 	fmt.Println(localString)
+}
+
+/**
+* @param1 int
+* @param2 int
+* @returnn int
+ */
+func OperasiMtkTambahan(a int, b int) int {
+	var hasil = a + b
+	return hasil
+
+}
+
+//coba keyword lain yak
+//ref https://golang.cafe/blog/golang-random-number-generator.html
+func randomAngka(min int, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	Angka := rand.Intn(max - min + 1 + min)
+	return Angka
+
 }
--- src/main.go
+++ src/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Print("hellow")
+	fmt.Print("helloworld XD")
 	var slicenih = []string{
 		"aduh",
 		"katanya",
@@ -24,5 +24,17 @@ func main() {
 	}
 	//join array string with function in another file
 	setStringMessages(arrString)
+
+	// mau coba fungsi pake return value nih ,
+	hasilnya := OperasiMtkTambahan(1, 1)
+
+	fmt.Println(hasilnya) //harusnya si hasilnya 2 XD
+
+	//coba random angka
+	var count int = 100
+	for index := 1; index < count; index++ {
+		angkaRandom := randomAngka(index, index)
+		fmt.Println("dapet berapa ya => ", angkaRandom)
+	}
 
 }
