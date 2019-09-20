52d0bf8c77b856c21d358be638a36043b386b6e0
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-09-20 10:22:29 +0700

    reflect

--- goreflect.go
+++ goreflect.go
@@ -15,5 +15,30 @@ func cobaReflect() {
 	}
 	fmt.Println("", checkRef.Interface())
 	var nilaiVarnya = checkRef.Interface().(int)
-	fmt.
+	fmt.Println("value nilaiVarnya=>", nilaiVarnya)
+}
+
+type Students struct {
+	Name  string
+	Grade int
+}
+
+func (s *Students) getPropertyInfo() {
+	var reflectValue = reflect.ValueOf(s)
+	fmt.Println(reflectValue)
+	if reflectValue.Kind() == reflect.Ptr {
+		reflectValue = reflectValue.Elem()
+	}
+
+	var reflectType = reflectValue.Type()
+	for i := 0; i < reflectValue.NumField(); i++ {
+		fmt.Println("nama\t:", reflectType.Field(i).Name)
+		fmt.Println("tipe data\t:", reflectType.Field(i).Type)
+		fmt.Println("nilai\t:", reflectValue.Field(i).Interface())
+
+	}
+}
+func (s *Students) SetName(name string, grade int) {
+	s.Name = name
+	s.Grade = grade
 }
